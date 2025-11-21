969669cb6e75a08c1b067cbb9e86d922efe1efc6
RAISSA MARA ALVES GOMES SANTANA <020.720092@universidadesenaicimatec.edu.br>
2025-11-21 14:29:20 -0300

    Incluída na rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path:"home",component:HomeComponent},
     {path:"dashboard",component:DashboardComponent},
+    {path:"login",component:LoginComponent},
     {path:"", redirectTo:"Login",pathMatch:"full"}
 ];
