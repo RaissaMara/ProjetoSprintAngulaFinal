d9cfb0bf8d832f6f8a157cce126790c89259f000
RAISSA MARA ALVES GOMES SANTANA <020.720092@universidadesenaicimatec.edu.br>
2025-11-21 17:02:16 -0300

    Incluir API e a Lógica da página de login (semi-finalizada)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { AuthService } from '../../services/auth.service'
+import { FormsModule } from '@angular/forms'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  usuario={
+    nome:'',
+    senha:''
+  }
+
+  constructor(private authService:AuthService, private router:Router){}
+
+  login(){
+  this.authService.login(this.usuario).subscribe({
+  next:(response)=>{
+    
+    console.log("Usuário efetuado");
+    this.router.navigate(["/home"]);
+  },
+  error:(err)=>{
+    alert("usuário ou senha incorreta");
+  }
+
+})
+
+  }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  private apiUrl = 'http://localhost:3001';
+
+  constructor(private http:HttpClient) { }
+
+  login(usuario:Pick<Usuario, 'nome'|'senha'>):Observable<Usuario>{
+    return this.http.post<Usuario>(`${this.apiUrl}/login`,usuario);
+  }
+
+}
